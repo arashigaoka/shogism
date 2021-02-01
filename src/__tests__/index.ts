8f69d82b0db4d8b23bffede4f7fa5f23b2401586
yuta.okumura <yuta.okumura@linecorp.com>
2021-02-01 18:40:54 +0900

    piece selector

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,4 +1,4 @@
-import { initializeBoard, flip, toPrettierString } from '../index';
+import { initializeBoard, flip, toPrettierString, selectPiece } from '../index';
 
 describe('flip', () => {
   test('flip', () => {
@@ -23,5 +23,27 @@ PPPPPPPPP
 LNSGKGSNL
 `;
     expect(toPrettierString(board)).toBe(prettierString);
+  });
+  test('select piece', () => {
+    const sfenInput =
+      'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL';
+    const board = initializeBoard(sfenInput);
+    expect(selectPiece(board, 0)).toBe('l');
+    expect(selectPiece(board, 80)).toBe('L');
+    expect(selectPiece(board, { x: 9, y: 1 })).toBe('l');
+    expect(selectPiece(board, { x: 7, y: 7 })).toBe('P');
+    expect(selectPiece(board, { x: 9, y: 8 })).toBe('');
+    function selectPieceWithInvalidNum() {
+      selectPiece(board, 81);
+    }
+    expect(selectPieceWithInvalidNum).toThrowError(
+      'selected Position is out of bounds',
+    );
+    function selectPieceWithInvalidPoint() {
+      selectPiece(board, { x: 0, y: 2 });
+    }
+    expect(selectPieceWithInvalidPoint).toThrowError(
+      'selected Position is out of bounds',
+    );
   });
 });
