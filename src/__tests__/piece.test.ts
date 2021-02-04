67710dcc5cf3bbf194d877c0858219083512b739
arashigaoka <bullet_of_lollypop@icloud.com>
2021-02-04 15:38:19 +0900

    get changeable piece

--- src/__tests__/piece.test.ts
+++ src/__tests__/piece.test.ts
@@ -1,4 +1,4 @@
-import { flip } from '../piece';
+import { flip, getChangeablePiece } from '../piece';
 
 describe('flip', () => {
   test('promote', () => {
@@ -8,5 +8,23 @@ describe('flip', () => {
   test('unpromote', () => {
     const flippedPorn = flip('P');
     expect(flippedPorn).toBe('p');
+  });
+});
+describe('getChangeablePiece', () => {
+  test('success', () => {
+    const changeablePiece = getChangeablePiece('p');
+    expect(changeablePiece).toStrictEqual(['p', 'P', '+p', '+P']);
+  });
+  test('only one result when argument is Gold', () => {
+    const changeablePiece = getChangeablePiece('k');
+    expect(changeablePiece).toStrictEqual(['k', 'K']);
+  });
+  test('argument is upperCase', () => {
+    const changeablePiece = getChangeablePiece('P');
+    expect(changeablePiece).toStrictEqual(['p', 'P', '+p', '+P']);
+  });
+  test('argument is promoted piece', () => {
+    const changeablePiece = getChangeablePiece('+P');
+    expect(changeablePiece).toStrictEqual(['p', 'P', '+p', '+P']);
   });
 });
