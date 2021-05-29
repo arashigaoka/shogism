b225e25198cca43f85a7b0caf39240b98a62daeb
arashigaoka <bullet_of_lollypop@icloud.com>
2021-05-29 17:52:12 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './board';
 export * from './piece';
+export * from './parser';
+export * from './kifu';
---
+++ src/parser/index.ts
@@ -0,0 +1,3 @@
+export * from './common';
+export * from './ki2Parser';
+export * from './kifParser';
